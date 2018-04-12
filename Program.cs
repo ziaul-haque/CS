94c3779cf746799133e52512ee608131d42cfc7e
mac <ziaul.ali@gmail.com>
2018-04-12 22:18:34 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Parent{
+
+        public String Name {get;private set;}
+        public Parent(string name){
+            Name = name;
+        }
+        public virtual void GreetingMember(string memberName){
+            Console.WriteLine("Hello " + memberName + ", I am " + Name);
+        }
+        public void GreetingHost(){
+            Console.WriteLine("Welcome to the world of Drama, I am " + Name);
+        } 
+    }
+    public class Child : Parent{
+        public Child(){
+            base("child");
+        }
+    }
+}
