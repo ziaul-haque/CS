c90742feeb708a7bd11d6898aff7f3eb49f37369
mac <ziaul.ali@gmail.com>
2018-04-12 22:22:12 +0530

    Update code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace CS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Inheritence!");
+
+            Parent parent = new Parent("Parent");
+            parent.GreetingMember("Pankaj");
+            parent.GreetingHost();
+
+            Child child = new Child();
+            child.GreetingMember("Pankaj");
+            child.GreetingHost();
+
+            parent = child;
+            parent.GreetingMember("Pankaj");
+            parent.GreetingHost();
+
+            
+
         }
     }
     public class Parent{
@@ -23,8 +38,15 @@ namespace CS
         } 
     }
     public class Child : Parent{
-        public Child(){
-            base("child");
+        public Child() : base("child"){
+            
+        }
+        public override void GreetingMember(string memberName){
+            Console.WriteLine("Hello " + memberName + ", I am " + Name);
+        }
+
+        public new void GreetingHost(){
+            Console.WriteLine("Welcome to the world of Drama, I am " + Name);   
         }
     }
 }
